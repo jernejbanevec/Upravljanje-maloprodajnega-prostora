88abfc0d17bba51c07bcb28a9a7332c2e4114006
LeonHorvat <leon.horvat1@gmail.com>
2017-12-29 20:06:14 +0100

    Dodana funkcija DSS

--- algoritem_dodeljen_prostor/algoritem_dodeljen_prostor.py
+++ algoritem_dodeljen_prostor/algoritem_dodeljen_prostor.py
@@ -2,16 +2,16 @@ import numpy
 import math
 import pulp
 
-def bisekcija_dodeljen_prostor(f,a,b,eps=0.01):
+
+def bisekcija_dodeljen_prostor(f, a, b, eps=0.01):
     c = a[:]
     c[0] = (a[0]+b[0])/2
-    d = a[0]
     if abs(a[0]-b[0]) < eps:
         return a[0]
-    elif f(a[0], a[1], a[2],a[3],a[4],a[5],a[6],a[7])*f(c[0], a[1], a[2],a[3],a[4],a[5],a[6],a[7])<0:
-        return bisekcija_dodeljen_prostor(f,a,c,eps)
+    elif f(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7])*f(c[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7]) < 0:
+        return bisekcija_dodeljen_prostor(f, a, c, eps)
     else:
-        return bisekcija_dodeljen_prostor(f,c,b,eps)
+        return bisekcija_dodeljen_prostor(f, c, b, eps)
 
 def opt_T(lam, y, s, th, c, H, k):
     return [((x0 * x1) / (x2 * ((1 + x3) * x4 * lam + x5))) ** 0.5 for x0, x1, x2, x3, x4, x5 in zip(k, y, s, th, c, H)]
@@ -23,14 +23,24 @@ def pogoj_prostor(lam, y, s, th, c, H, k, C):
         z += (1 + th[i]) * c[i] * s[i] * T[i]
     return z - C
 
-def resi_DSS(d,v,k,th,C,c,w,H,T)
+def resi_DSS(d, v, k, th, C, c, w, H, T):
+    n = len(v)
     DSS = pulp.LpProblem('DSS', pulp.LpMaximize)
-    y = pulp.LpVariable('y', range(len(v)), lowBound=0,upBound=1)
-    x = pulp.LpVariable('x', range(len(v)), lowBound=0)
-    DSS += pulp.lpSum(v[i]*s[i] for i in range(len(v))) - sum(H[i]*v[i]*T[i] for i in range(len(s))) - sum(k[i]*y[i] for i in range(len(v))), 'Z'
-    for i in range(len(s)):
-        DSS += s[i] == sum(w[i][j]*d[j]*x[i][j])
-
+    y = [pulp.LpVariable('y%d' % i, lowBound=0, upBound=1) for i in range(n)]
+    x = [[pulp.LpVariable("x%d,%d" % (i, j), lowBound=0) for j in range(n)] for i in range(n)]
+    s = []
+    for i in range(n):
+        s.append(sum(w[i][j]*d[j]*x[i][j] for j in range(n)))
+    DSS += pulp.lpSum(v[i]*s[i] - H[i]*v[i]*T[i] - k[i]*y[i] for i in range(n)), 'Z'
+    for i in range(n):
+        for j in range(n):
+            DSS += x[i][j] <= y[i]
+            DSS += x[i][j] <= 1 - y[i]
+    DSS += pulp.lpSum((1+th[i])*c[i]*s[i]*T[i] for i in range(n)) <= C
+    DSS.solve()
+    for v in DSS.variables():
+        print(v.name, v.varValue)
+    print(pulp.value(DSS.objective))
 
 def dodeljen_prostor(d,v,k,th,C,c,eps):
     H = [a*(0.5 + b) for a,b in zip(c,th)]
