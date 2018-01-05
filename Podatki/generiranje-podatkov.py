e86e9e6a0d6840b30eb1ebb6a80bc45f16705b7a
LeonHorvat <leon.horvat1@gmail.com>
2018-01-05 22:36:52 +0100

    komentiranje kode

--- Podatki/generiranje-podatkov.py
+++ Podatki/generiranje-podatkov.py
@@ -3,19 +3,19 @@
 import random as rd
 import math
 
-def generiraj_podatke(N = 2 + round(8 * round(rd.uniform(0, 1), 2))):
+def generiraj_podatke(N = 2 + round(13 * round(rd.uniform(0, 1), 2))):
+    #Funkcija generira naključne podatke za N izdelkov, če ne podamo vhodnega podatka, je tudi ta izbira naključna med 2 in 15.
     d = []
-    H = []
     theta = []
     CV = []
     v = []
     k = []
     c = []
-    U = round(rd.uniform(0, 1), 2)
+    U = round(rd.uniform(0, 1), 2) #naključno število med 0 in 1
     M = N
     C = (5 + 30 * U) * M
 
-    for i in range(0, M):
+    for i in range(0, M): #dopolnimo vse sezname z M naključnimi podatki
         U = round(rd.uniform(0, 1), 2)
         CV_B = 1 + 9 * U
         U = round(rd.uniform(0, 1), 2)
@@ -48,22 +48,18 @@ def generiraj_podatke(N = 2 + round(8 * round(rd.uniform(0, 1), 2))):
         U = round(rd.uniform(0, 1), 2)
         c.append(round(c2 + (U - 0.5)* delta_c,3))
 
-        H.append(round(c[i] * (0.5 + theta[i]),3))
-
-
-    w = [[round(rd.uniform(0, (1/M)), 3) for i in range(M)] for j in range(M)] #zaokroženo na 3 decimalke, da je predstavljivo
+    w = [[round(rd.uniform(0, (1/M)), 3) for i in range(M)] for j in range(M)] #matrika substitucije (zaokroženo na 3 decimalke, da je predstavljivo)
     for i in range(M):
         w[i][i] = 1
     return list((d,v,k,theta,C,c,w,H))
 
 
-
-#strategija_skupnega_prostora(x[0], x[1], x[2], x[3], x[4], x[5], x[6], x[7])
-
 def mean(numbers):
+    #Funkcija, ki vrne povprečje podanih števil (numbers)
     return float(sum(numbers)) / max(len(numbers), 1)
 
 def primerjava_povprecij(N):
+    #Funkcija, ki izračuna povprečje obeh strategij za 150 naključno podanih problemov, za 2 do N izdelkov.
     povprecje_ss = []
     povprecje_ds = []
     for j in range(2, N+1):
@@ -78,6 +74,7 @@ def primerjava_povprecij(N):
     return povprecje_ds, povprecje_ss
 
 def primerjava_porabe_protora(N):
+    #Funkcija, ki izračuna povprečje obeh strategij za 150 naključno generiranih problemov in različnimi C-ji za N izdelkov.
     povprecje_ds = []
     povprecje_ss = []
     for i in range(11):
